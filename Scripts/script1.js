c452142d82f4bb8d49595fed6f7d8a66e7aac8ea
G.R. Ali Quaderi <quaderinayeem@gmail.com>
2025-10-09 10:46:45 +0600

    messed up

--- Scripts/script1.js
+++ Scripts/script1.js
@@ -1,1 +1,5 @@
-console.log("Hello")
+const url='https://openapi.programming-hero.com/api/categories'
+const loadcatagories=() => {
+    fetch(url).then(res=>res.json()).then(data=>console.log(data))
+}
+loadcatagories();
