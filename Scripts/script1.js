e3f154146a5517cb1efedc34c23429aa9ba5bda4
G.R. Ali Quaderi <quaderinayeem@gmail.com>
2025-10-09 13:39:08 +0600

    added nav functionality

--- Scripts/script1.js
+++ Scripts/script1.js
@@ -1,5 +1,19 @@
-const url='https://openapi.programming-hero.com/api/categories'
-const loadcatagories=() => {
-    fetch(url).then(res=>res.json()).then(data=>console.log(data))
+//First the left nav part
+const loadLeftNav = () =>{fetch('https://openapi.programming-hero.com/api/categories')
+    .then(res => res.json())
+    .then(data => {
+        displayNavLeft(data.categories);
+    })
+};
+displayNavLeft=(categories)=>{
+    console.log(categories);
+    const containerNavLeft=document.getElementById('LeftNavContainer');
+    categories.forEach(category=>{
+        const child=document.createElement("div");
+        child.innerHTML=`${category.category_name}`
+        child.classList.add('left-nav','w-full');
+        containerNavLeft.appendChild(child)
+    })
 }
-loadcatagories();
+
+loadLeftNav();
