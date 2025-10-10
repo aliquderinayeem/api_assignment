b6ebbb6821d3eed482b1e4a02bb25035a24ceaea
G.R. Ali Quaderi <quaderinayeem@gmail.com>
2025-10-10 17:39:52 +0600

    Final touch

--- Scripts/script1.js
+++ Scripts/script1.js
@@ -206,7 +206,7 @@ const thanks=()=>{
 let totalMoney=document.getElementById("total_Money");
 let total=parseInt(totalMoney.innerText);
 const cartadded=(price,name)=>{
-    
+    alert(`Added ${name} to Cart\nPrice is ${price}`)
     const totalContainer=document.getElementById("cartContainer");
     const child=document.createElement("div");
     child.innerHTML=`
